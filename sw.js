abf4a7879d331bfb80cc3d7b432925813ef7c747
Bas1cally <102512510+Bas1cally@users.noreply.github.com>
2026-01-20 20:05:47 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-const CACHE_NAME = 'mbwd-app-v8';
+const CACHE_NAME = 'mbwd-app-v15';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './docs/054_CL_Corpuls3_EKG_Defibrillator.pdf',
+  './docs/054_CL_AccuvacRescue_Absaugpumpe.pdf',
+  './docs/054_CL_Medumat_Standard_Beatmungsgerät.pdf',
+  './docs/054_CL_MANV_Tasche_RTW.pdf',
+  './docs/054_CL_Notfallrucksäcke_Rot_Gelb.pdf',
+  './docs/054_CL_Notfallrucksack_Kinder.pdf'
 ];
 
 self.addEventListener('install', event => {
